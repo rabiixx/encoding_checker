2afa14349807f3faec274d1d0ccec8d50d43aab5
rabiixx <99rubenche@gmail.com>
2022-05-11 10:11:14 +0200

    Added main and utils

---
+++ check_utf8.ps1
@@ -0,0 +1,102 @@
+[CmdletBinding()] Param (
+    [Parameter(Mandatory = $True)] [string[]] $path,
+    [Parameter(Mandatory = $False)] [string[]] $only,
+    [Parameter(Mandatory = $False)] [string[]] $exclude,
+    [Parameter(Mandatory = $False)] [string[]] $excludepath
+)
+
+. .\"utils.ps1"
+
+$folder = Get-Item $path
+Write-Host "[+] Actual Folder: " $folder
+Write-Host "[+] Only: " $only
+Write-Host "[+] Exclude: " $exclude
+Write-Host "[+] Exclude Path: " $excludepath
+
+$is_set_only = $PSBoundParameters.ContainsKey('only')
+$is_set_exclude = $PSBoundParameters.ContainsKey('exclude')
+$is_set_excludepath = $PSBoundParameters.ContainsKey('excludepath')
+
+# Remove files defined on excludepath param
+function filter_excludepath( [System.Collections.ArrayList]$files ) {
+
+    foreach($file in $files.Where( { $_.FullName -in $excludepath } ) ) {
+        $files.Remove($file)
+    }
+
+    return $files
+}
+
+# Remove files with extension not defined on only param
+function filter_only( [System.Collections.ArrayList]$files ) {
+
+    foreach($file in $files.Where({ !($_.Extension -in $only) })) {
+        $files.Remove($file)
+    }
+
+    return $files
+
+}
+
+# Remove files with extension defined on exclude param
+function filter_exclude( [System.Collections.ArrayList]$files ) {
+
+    foreach($file in $files.Where({ !($_.Extension -in $exclude) })) {
+        $files.Remove($file)
+    }
+
+    return $files
+
+}
+
+[System.Collections.ArrayList]$files = Get-ChildItem $folder -Recurse -File
+
+# No tiene sentido usar el only junto con el exclude
+if ( $is_set_only -and $is_set_exclude ) {
+    Write-Host "[+] Parameters only and exclude cannot be used together"
+    exit
+}
+
+if ( $is_set_excludepath ) { $files = filter_excludepath($files) }
+
+if ( $is_set_only ) { $files = filter_only($files) }
+
+if ( $is_set_exclude ) { $files = filter_exclude($files) }
+
+Write-Host "`n`r[+] Filtered Files: "
+foreach ($file in $files) { $file.FullName }
+
+# [+] Determine encoding
+foreach( $file in $files ) {
+
+    Write-Host "[+] Checking " $file.FullName
+
+    # Get File Byte Array
+    [Byte[]]$bytes = Get-Content -Encoding Byte $file.Fullname
+
+    # Byte Array 2 Hex Array
+    $hex_array = ($bytes|ForEach-Object ToString X2)
+    $hex_array_size = $hex_array.Count
+
+    # Check encoding type
+    if ( Get-file-looks-utf8-with-BOM($file) ) {
+
+        $file_encoding = [Text.Encoding]::GetEncoding(65001).EncodingName + " with BOM"
+
+    } elseif ( $( is_utf8($hex_array, $hex_array_size, $msg, $faulty_bytes) ) -eq 0 ) {
+
+        $file_encoding = [Text.Encoding]::GetEncoding(65001).EncodingName + " without BOM"
+
+    } else {
+
+        Write-Host "`tMessage: " $msg
+        Write-Host "`tFaulty Bytes: " $faulty_bytes
+        $file_encoding = [Text.Encoding]::GetEncoding(20127).EncodingName + " (or unknown encoding)"
+
+    }
+
+    Write-Host "`t" $file.Fullname " ==> " $file_encoding -ForegroundColor Green
+
+}
+
+# .\check_utf8.ps1 -path .\wachin\ -only .txt -excludepath H:\Powershell\wachin\untitled1.txt, H:\Powershell\wachin\hack.hhh
---
+++ utils.ps1
@@ -0,0 +1,257 @@
+<#
+    http://www.unicode.org/versions/Unicode7.0.0/UnicodeStandard-7.0.pdf (page 124, 3.9 "Unicode Encoding Forms", "UTF-8")
+
+    https://sites.google.com/site/markusicu/unicode/utf-8-bytes
+
+    Table 3-7. Well-Formed UTF-8 Byte Sequences
+    -----------------------------------------------------------------------------
+    |  Code Points        | First Byte | Second Byte | Third Byte | Fourth Byte |
+    |  U+0000..U+007F     |     00..7F |             |            |             |
+    |  U+0080..U+07FF     |     C2..DF |      80..BF |            |             |
+    |  U+0800..U+0FFF     |         E0 |      A0..BF |     80..BF |             |
+    |  U+1000..U+CFFF     |     E1..EC |      80..BF |     80..BF |             |
+    |  U+D000..U+D7FF     |         ED |      80..9F |     80..BF |             |
+    |  U+E000..U+FFFF     |     EE..EF |      80..BF |     80..BF |             |
+    |  U+10000..U+3FFFF   |         F0 |      90..BF |     80..BF |      80..BF |
+    |  U+40000..U+FFFFF   |     F1..F3 |      80..BF |     80..BF |      80..BF |
+    |  U+100000..U+10FFFF |         F4 |      80..8F |     80..BF |      80..BF |
+    -----------------------------------------------------------------------------
+#>
+function is_utf8($hex_array, $hex_array_size, [ref]$msg, [ref]$faulty_bytes) {
+
+    $i = 0
+    $msg = $null
+    $faulty_bytes = 0
+    while ($i -lt $hex_array_size)
+    {
+        if ($hex_array[$i] -le 0x7F) # 00..7F */
+        {
+            $i += 1
+        }
+        elseif ($hex_array[$i] -ge 0xC2 -and $hex_array[$i] -le 0xDF) # C2..DF 80..BF */
+        {
+            if ($i + 1 -lt $hex_array_size) # Expect a 2nd byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x80 -or $hex_array[$i + 1] -gt 0xBF)
+                {
+                    $msg = "After a first byte between C2 and DF, expecting a 2nd byte between 80 and BF"
+                    $faulty_bytes = 2
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte between C2 and DF, expecting a 2nd byte."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 2
+        }
+        elseif ($hex_array[$i] -eq 0xE0) # E0 A0..BF 80..BF */
+        {
+            if ($i + 2 -lt $hex_array_size) # Expect a 2nd and 3rd byte */
+            {
+                if ($hex_array[$i + 1] -lt 0xA0 -or $hex_array[$i + 1] -gt 0xBF)
+                {
+                    $msg = "After a first byte of E0, expecting a 2nd byte between A0 and BF."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte of E0, expecting a 3nd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte of E0, expecting two following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 3
+        }
+        elseif ($hex_array[$i] -ge 0xE1 -and $hex_array[$i] -le 0xEC) # E1..EC 80..BF 80..BF */
+        {
+            if ($i + 2 -lt $hex_array_size) # Expect a 2nd and 3rd byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x80 -or $hex_array[$i + 1] -gt 0xBF)
+                {
+                    $msg = "After a first byte between E1 and EC, expecting the 2nd byte between 80 and BF."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte between E1 and EC, expecting the 3rd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte between E1 and EC, expecting two following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 3
+        }
+        elseif ($hex_array[$i] -eq 0xED) # ED 80..9F 80..BF */
+        {
+            if ($i + 2 -lt $hex_array_size) # Expect a 2nd and 3rd byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x80 -or $hex_array[$i + 1] -gt 0x9F)
+                {
+                    $msg = "After a first byte of ED, expecting 2nd byte between 80 and 9F."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte of ED, expecting 3rd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte of ED, expecting two following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 3
+        }
+        elseif ($hex_array[$i] -ge 0xEE -and $hex_array[$i] -le 0xEF) # EE..EF 80..BF 80..BF */
+        {
+            if ($i + 2 -lt $hex_array_size) # Expect a 2nd and 3rd byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x80 -or $hex_array[$i + 1] -gt 0xBF)
+                {
+                    $msg = "After a first byte between EE and EF, expecting 2nd byte between 80 and BF."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte between EE and EF, expecting 3rd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte between EE and EF, two following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 3
+        }
+        elseif ($hex_array[$i] -eq 0xF0) # F0 90..BF 80..BF 80..BF */
+        {
+            if ($i + 3 -lt $hex_array_size) # Expect a 2nd, 3rd 3th byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x90 -or $hex_array[$i + 1] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F0, expecting 2nd byte between 90 and BF."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F0, expecting 3rd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+                if ($hex_array[$i + 3] -lt 0x80 -or $hex_array[$i + 3] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F0, expecting 4th byte between 80 and BF."
+                    $faulty_bytes = 4
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte of F0, expecting three following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 4
+        }
+        elseif ($hex_array[$i] -ge 0xF1 -and $hex_array[$i] -le 0xF3) # F1..F3 80..BF 80..BF 80..BF */
+        {
+            if ($i + 3 -lt $hex_array_size) # Expect a 2nd, 3rd 3th byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x80 -or $hex_array[$i + 1] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F1, F2, or F3, expecting a 2nd byte between 80 and BF."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F1, F2, or F3, expecting a 3rd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+                if ($hex_array[$i + 3] -lt 0x80 -or $hex_array[$i + 3] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F1, F2, or F3, expecting a 4th byte between 80 and BF."
+                    $faulty_bytes = 4
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte of F1, F2, or F3, expecting three following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 4
+        }
+        elseif ($hex_array[$i] -eq 0xF4) # F4 80..8F 80..BF 80..BF */
+        {
+            if ($i + 3 -lt $hex_array_size) # Expect a 2nd, 3rd 3th byte */
+            {
+                if ($hex_array[$i + 1] -lt 0x80 -or $hex_array[$i + 1] -gt 0x8F)
+                {
+                    $msg = "After a first byte of F4, expecting 2nd byte between 80 and 8F."
+                    $faulty_bytes = 2
+                    return $i
+                }
+                if ($hex_array[$i + 2] -lt 0x80 -or $hex_array[$i + 2] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F4, expecting 3rd byte between 80 and BF."
+                    $faulty_bytes = 3
+                    return $i
+                }
+                if ($hex_array[$i + 3] -lt 0x80 -or $hex_array[$i + 3] -gt 0xBF)
+                {
+                    $msg = "After a first byte of F4, expecting 4th byte between 80 and BF."
+                    $faulty_bytes = 4
+                    return $i
+                }
+            }
+            else
+            {
+                $msg = "After a first byte of F4, expecting three following bytes."
+                $faulty_bytes = 1
+                return $i
+            }
+            $i += 4
+        }
+        else
+        {
+            $msg = "Expecting bytes in the following ranges: 00..7F C2..F4."
+            $faulty_bytes = 1
+            return $i
+        }
+    }
+    $msg = $null
+    return 0
+}
+
+function Get-file-looks-utf8-with-BOM($file) {
+    [Byte[]]$bom = Get-Content -Encoding Byte -ReadCount 4 -TotalCount 4 $file.Fullname
+    return ($bom.length -gt 3 -And $bom[0] -eq 0xef -and $bom[1] -eq 0xbb -and $bom[2] -eq 0xbf)
+}
